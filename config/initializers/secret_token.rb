d8eadae06a2520e483ec69ef2c50754e83768ea9
Tosswill <christosswill@gmail.com>
2011-11-04 16:29:59 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Glow::Application.config.secret_token = '624937afdd0c05b66a35d4c0936d4da914ba69e1afbdfdef8d0e03630c8704f1413b41a7391d8a727e707ad9b33f3fc5d9cfe412fa9ef06e68ff49ebbdb51237'
